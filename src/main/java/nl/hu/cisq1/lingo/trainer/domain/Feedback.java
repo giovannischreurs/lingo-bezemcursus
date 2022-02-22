f3eb259d47fcc3da8b755dca74b8d9d336437551
giovannischreurs <31764837+giovannischreurs@users.noreply.github.com>
2022-02-22 23:43:38 +0100

    FeedbackTest toegevoegd en succesvol gedraait

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,5 +1,6 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import javax.persistence.ElementCollection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -7,42 +8,17 @@ import java.util.function.Predicate;
 
 public class Feedback {
     private String attempt;
-    private List<Mark> markList;
 
-    public Feedback(String attempt, List<Mark> markList) {
+    @ElementCollection
+    private List<Mark> marks;
+
+    public Feedback(String attempt, List<Mark> marks) {
         this.attempt = attempt;
-        this.markList = markList;
-    }
-
-    public String giveHint(String formerHint) {
-        String hint = "";
-
-        if (!formerhint.isBlank()) {
-            hint = formerHint;
-        }
-
-        else {
-            for (int wordsize = 0 ; wordsize<attempt.length() ; wordsize++) {
-                hint+="-";
-            }
-        }
-
-        char[] hintCharList = hint.toCharArray();
-        int index = 0;
-        for (Mark mark: markList) {
-
-            if(mark==Mark.CORRECT) {
-                hintCharList[index] = attempt.charAt(index);
-                hint = String.valueOf(hintCharList);
-            }
-            index+=1;
-
-        }
-        return hint;
+        this.marks = marks;
     }
 
     public boolean isWordGuessed() {
-        return markList.stream().allMatch(result -> result == Mark.CORRECT);
+        return marks.stream().allMatch(mark -> mark == Mark.CORRECT);
     }
 
 }
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -3,13 +3,20 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static nl.hu.cisq1.lingo.trainer.domain.Mark.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeedbackTest {
     @Test
     @DisplayName("Word is guessed if all letters are correct")
     public void wordIsGuessed() {
-        WordIsGuessed Feedback = new WordIsGuessed();
+        List<Mark> marks = List.of(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT);
+        String attempt = "STEEN";
+        Feedback feedback = new Feedback(attempt, marks);
+
+        assertTrue(feedback.isWordGuessed());
     }
 
 }
